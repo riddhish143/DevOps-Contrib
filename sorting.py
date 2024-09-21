dddc4d64cff2b5fb4cfb193e709e6566572244d3
Varun Samaga B L <varunsamagabl@gmail.com>
2024-09-21 11:25:58 +0530

    Added Counting sort

--- sorting.py
+++ sorting.py
@@ -86,7 +86,23 @@ def heap_sort(arr):
 
 def counting_sort(arr):
     """Counting Sort Algorithm"""
-    pass
+
+    M = max(arr)
+    count_array = [0] * (M + 1)
+
+    for num in arr:
+        count_array[num] += 1
+
+    for i in range(1, M + 1):
+        count_array[i] += count_array[i - 1]
+
+    output_array = [0] * len(arr)
+
+    for i in range(len(arr) - 1, -1, -1):
+        output_array[count_array[arr[i]] - 1] = arr[i]
+        count_array[arr[i]] -= 1
+
+    return output_array
 
 
 def bucket_sort(array):
